feb239f311bb937124c12cf89e2c252576e80446
Lowtrack <popovvs86@gmail.com>
2014-08-07 14:56:24 +0400

    Initial Commit

---
+++ Autocomplete TextField/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Autocomplete TextField
+//
+//  Created by Lowtrack on 07.08.14.
+//  Copyright (c) 2014 AR for YOU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Autocomplete TextField/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Autocomplete TextField
+//
+//  Created by Lowtrack on 07.08.14.
+//  Copyright (c) 2014 AR for YOU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
